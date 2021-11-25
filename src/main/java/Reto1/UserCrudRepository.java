23d1905e434d67b2b72da354fe64cc13b69de1d5
vijaropin <vijaro-4@hotmail.com>
2021-11-25 13:35:57 -0500

    Reto1_clon

---
+++ src/main/java/Reto1/UserCrudRepository.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Reto1;
+
+import java.util.Optional;
+import org.springframework.data.repository.CrudRepository;
+
+
+
+/**
+ *
+ * @author Usuario
+ */
+
+public interface UserCrudRepository  extends CrudRepository <User, Integer> {
+    Optional<User> findByEmail (String email);
+    Optional<User> findByEmailAndPassword(String email, String password);
+   
+}
